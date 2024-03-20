0ccd78404c636347f739cb95b3f3ff1cb229cbb6
vanthom04 <vanthom2210@gmail.com>
2024-03-20 18:35:41 +0700

    update source code

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/music-app-web',
+  base: '/',
   resolve: {
     alias: [{ find: '~', replacement: '/src' }]
   },

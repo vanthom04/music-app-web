aa76cd1e18dc50664e8fbeab95daf52f0a13a9ca
vanthom04 <vanthom2210@gmail.com>
2024-03-20 18:52:37 +0700

    update source code

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   resolve: {
     alias: [{ find: '~', replacement: '/src' }]
   },

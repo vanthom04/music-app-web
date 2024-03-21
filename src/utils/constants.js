f99f7fd3f00be1da37e6aed9b5935cbcd0b7b766
vanthom04 <vanthom2210@gmail.com>
2024-03-22 02:47:28 +0700

    update api root

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,9 @@
 // constants
 
+export const API_ROOT = import.meta.env.DEV
+  ? 'http://localhost:3150/api/v1'
+  : 'https://music-app-api-umber.vercel.app/api/v1'
+
 export const padStart = (value, length) => {
   return String(value).padStart(length, '0')
 }
